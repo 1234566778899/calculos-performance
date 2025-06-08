079e97aa9c6d1b178f11428c63372b3141e8887a
Carlos Jesús Ordaz Hoyos <ordazhoyos2001@gmail.com>
2025-06-08 15:16:40 -0500

    xx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="">
             <div className="w-full flex">
                 <button onClick={() => navigate('/a')} className="w-full py-10 bg-slate-400 hover:brightness-95">Velocidades</button>
-                <button onClick={() => navigate('/b')} className="w-full py-10 bg-slate-500 text-white hover:brightness-95">ERP</button>
+                <button onClick={() => navigate('/b')} className="w-full py-10 bg-slate-500 text-white hover:brightness-95">EPR</button>
                 <button onClick={() => navigate('/c')} className="w-full py-10 bg-slate-600 text-white hover:brightness-95">Peso y Balance</button>
             </div>
             <br />
